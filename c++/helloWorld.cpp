0f44bf786f7c23226b6d6c3f9aadb4b795b4d3c4
Tim Jones <31971670+t583046@users.noreply.github.com>
2020-05-02 20:16:53 -0400

    Add files via upload

---
+++ c++/helloWorld.cpp
@@ -0,0 +1,18 @@
+//This is a simple Hello World Program
+/*First, we include our headers. This allows us to utilize the 
+many different libraries and functions within those libraries
+for our program*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //here we will print the phrase hello world using the "cout" statement.
+    //part of the ostream class
+    cout << endl << "Hello World" << endl;
+
+
+
+    return 0;
+}
